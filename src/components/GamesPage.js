2f60f7531fcaa106ac19826c2f1ec8cb8ed9b3d8
kitety <aizaizuori@foxmail.com>
2018-11-01 23:35:27 +0800

    react路由

---
+++ src/components/GamesPage.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class GamesPage extends Component {
+  render() {
+    return (
+      <div >
+        games Page1212
+      </div>
+    );
+  }
+}
+
+export default GamesPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GamesPage from './components/GamesPage';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import reducers from './reducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import {logger} from 'redux-logger'
+import { logger } from 'redux-logger'
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(logger)))
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div className="ui container">
+        <div className="ui three item menu">
+          <NavLink exact activeClassName="active" className="item" to="/">Home</NavLink>
+          <NavLink exact activeClassName="active" className="item" to="/games">Games</NavLink>
+          <NavLink exact activeClassName="active" className="item" to="/games/new">Add New Games</NavLink>
+          {/*   <a href="#" className="item">Home</a>
+          <a href="#" className="item">Games</a>
+          <a href="#" className="item">Add New Games</a> */}
+        </div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/games" component={GamesPage} />
+        <Route path="/games/new" component={GamesPage} />
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root'));
 
