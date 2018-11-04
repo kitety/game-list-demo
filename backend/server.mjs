d1e62c3e1f696c4db703ef8b628ec1e3709d22ef
kitety <aizaizuori@foxmail.com>
2018-11-04 16:05:20 +0800

    本地数据库的链接,注意重启

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,6 +1,21 @@
 import express from 'express'
+import mongodb from 'mongodb';
 // const express = require('express')
 var app = new express()
-app.listen(8080, () => {
-  console.log('The server is listening on: 8080!');
+const dbURL = 'mongodb://localhost'
+mongodb.MongoClient.connect(dbURL, (err, client) => {
+  if (err) {
+    throw err
+  }
+  console.log('数据库创建!');
+  const db = client.db('crud')
+  app.get('/api/games', (req, res) => {
+    // find --select
+    db.collection('games').find({}).toArray((err, games) => {
+      res.json({ games })
+    })
+  })
+  app.listen(8080, () => {
+    console.log('The server is listening on: 8080!');
+  })
 })
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,15 @@
+import { SET_GAMES } from '../constants';
+const setGames = (games) => {
+  return {
+    type: SET_GAMES,
+    games
+  }
+}
 export const fetchGames = () => {
   // 返回函数
   return dispatch => {
     fetch('/api/games')
+      .then(res => res.json())
+      .then(data => dispatch(setGames(data.games)))
   }
 }
