7681aa0cd21ac5e2ddb6994be8461b72675e26ff
kitety <aizaizuori@foxmail.com>
2018-11-04 17:11:33 +0800

    客户端验证

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
+import classnames from 'classnames'
 
 class GameForm extends Component {
   // 初始化数据
   state = {
     title: '',
-    cover: ''
+    cover: '',
+    errors: {}
   }
-  // 两种都可以
-  handChange(e) {
-    this.setState({ [e.target.name]: e.target.value })
+  handChange = (e) => {
+    let errors = Object.assign({}, this.state.errors)
+    if (!!errors[e.target.name]) {
+      console.log(1);
+      delete errors[e.target.name]
+      this.setState({
+        [e.target.name]: e.target.value,
+        errors
+      })
+    } else {
+      console.log(0)
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
   }
-  // handChange = (e) => {
-  //   console.log(e);
-  //   this.setState({ [e.target.name]: e.target.value })
-  // }
-  handSubmit(event) {
+  handSubmit = (event) => {
     event.preventDefault();
-  }
+    let errors = {}
+    if (this.state.title === '') errors.title = "The title can't be empty"
+    if (this.state.cover === '') errors.cover = "The image can't be empty"
+    this.setState({ errors })
+  };
   render() {
     return (
       <form className="ui form" onSubmit={this.handSubmit}>
         <h1>Add new Game</h1>
-        <div className="field">
+        <div className={classnames("field", { 'error': !!this.state.errors.title })}>
           <label htmlFor="title">Title</label>
           <input
             type="text"
@@ -29,8 +43,9 @@ class GameForm extends Component {
             value={this.state.text}
             onChange={this.handChange}
           />
+          <span>{this.state.errors.title}</span>
         </div>
-        <div className="field">
+        <div className={classnames("field", { 'error': !!this.state.errors.cover })}>
           <label htmlFor="title">Cover URL</label>
           <input
             type="text"
@@ -38,6 +53,7 @@ class GameForm extends Component {
             value={this.state.cover}
             onChange={this.handChange}
           />
+          <span>{this.state.errors.cover}</span>
         </div>
         <div className="field">
           {/* 前面满足才做后面 */}
