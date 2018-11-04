4bbb945318edd96b50d427eed7a5a5c5b6e4d5ed
kitety <aizaizuori@foxmail.com>
2018-11-04 14:53:31 +0800

    修改为require

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import express from 'express'
-// const express = require('express')
+// import express from 'express'
+const express = require('express')
 var app=new express()
 app.listen(8080,()=>{
   console.log('The server is listening on: 8080!');
